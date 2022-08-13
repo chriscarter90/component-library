685fcdac708770a6b50a8dcfe24bbde1592e88f0
Chris Carter <chris.carter907@gmail.com>
2022-08-13 21:41:37 +0100

    Update browsers and specs

--- src/CrystalStack/CrystalStack.spec.ts
+++ src/CrystalStack/CrystalStack.spec.ts
@@ -1,17 +1,10 @@
 import { shallowMount } from "@vue/test-utils";
 import CrystalStack from "./CrystalStack.vue";
 
-describe("SampleComponent", () => {
+describe("CrystalStack", () => {
   it("should render text correctly", () => {
-    const header = "header text";
-    const body = "body text";
-    const wrapper = shallowMount(CrystalStack, {
-      props: { headingText: header, bodyText: body }
-    });
+    const wrapper = shallowMount(CrystalStack);
 
-    const headerText = wrapper.find("h2");
-    const bodyText = wrapper.find("p");
-    expect(headerText.text()).toMatch(header);
-    expect(bodyText.text()).toMatch(body);
+    expect(wrapper.findAll('.stack')).toHaveLength(1)
   });
 });
